e077f7b5f8d4ec288d76535f40501be4a01b3739
musiur <musiur.opu@gmail.com>
2023-09-12 20:55:07 +0600

    font-size of heading modifie

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -8,7 +8,7 @@ export default async function Home({ params }: ParamLang) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-3 md:p-24">
       <h1
-        className={`text-5xl font-extrabold max-w-[800px] leading-[3.5rem] text-center ${
+        className={`text-2xl md:text-5xl font-extrabold max-w-[800px] leading-[3.5rem] text-center ${
           params.lang === "en" ? "text-blue-900" : "text-green-800"
         }`}
       >
